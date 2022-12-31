6ff90d96a9b4e7dfe72c35e1b6b5c4131bf7d49a
Your Name <hossein.moosavi78@gmail.com>
2022-12-31 19:07:24 +0330

    remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStoreHook} from 'react-redux';
-import TaskkArray from './components/redux/reducer/taskArray';
 
 
 const root = ReactDOM.createRoot(
